8597d8770e0a5f56accfbb90037a7fd9abe961bd
DiDinhKhoi <didinhkhoi.tdc2020@gmail.com>
2024-10-01 15:19:35 +0700

    labs 3

--- security/11-training-php/delete_user.php
+++ security/11-training-php/delete_user.php
@@ -1,13 +1,24 @@
 <?php
+session_start();
 require_once 'models/UserModel.php';
 $userModel = new UserModel();
-
-$user = NULL; //Add new user
-$id = NULL;
-
-if (!empty($_GET['id'])) {
-    $id = $_GET['id'];
-    $userModel->deleteUserById($id);//Delete existing user
+$allowed_origin = "http://localhost:8080";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    if (!isset($_SERVER['HTTP_REFERER']) || parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) !== parse_url($allowed_origin, PHP_URL_HOST)) {
+        die("Yêu cầu không hợp lệ.");
+    }
+ 
+    if (!empty($_POST['user_id'])) {
+        $id = $_POST['user_id'];
+        $userModel->deleteUserById($id); 
+    }
+    
+    header('Location: list_users.php');
+    exit(); 
+} else {
+    
+    header('Location: list_users.php');
+    exit();
 }
-header('location: list_users.php');
 ?>
