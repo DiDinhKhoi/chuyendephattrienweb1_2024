ee5348bace23cd986aa2fe27348de193ef4e001d
DiDinhKhoi <didinhkhoi.tdc2020@gmail.com>
2024-10-17 13:07:13 +0700

    labs4

---
+++ front/labs4/3077-content.php
@@ -0,0 +1,80 @@
+<div class="container-fluid">
+    <div class="row footer-container">
+      
+        <div class="col-md-3 footer-section">
+            <img src="footer-logo.png" alt="">
+            <p>We strive to help people by providing extraordinary service and expert repairs using only the highest quality
+                parts available.</p>
+            <p>We service all the newest and <span>popular mobile phones, tablets and laptops.</span>natis sed id nisl magna
+                auris et neque sollicitudin ullamcorper fusce molestie felis mi id.Vestibulum venenatis sed id nisl magna
+                suspendisse a mauris.</p>
+        </div>
+        
+        <div class="col-md-3 footer-section">
+            <h2>CONTACT US</h2>
+            <p>136 Kingston Road SW19 1LY Wimbledon London.</p>
+            <p>Monday - Friday: 9am - 6pm<br>Saturday: 9am - 4pm<br>Sunday: Appointments only.</p>
+            <p>Office: 0208 543 7088<br>Mobile: 0 778 35 166 93</p>
+        </div>
+        
+        <div class="col-md-4 footer-section">
+            <h2>FIXIT PAGES</h2>
+            <ul>
+                <li class="icon-circle"><a href="#">About us</a></li>
+                <li class="icon-circle"><a href="#">Repair Team</a></li>
+                <li class="icon-circle"><a href="#">Our Locations</a></li>
+                <li class="icon-circle"><a href="#">Appointment</a></li>
+                <li class="icon-circle"><a href="#">Contact us</a></li>
+                <li class="icon-circle"><a href="#">Career</a></li>
+            </ul>
+        </div>
+      
+        <div class="col-md-4 footer-section">
+            <h2>GET A REPAIR STARTED</h2>
+            <ul>
+                <li class="icon-circle"><a href="#">PC & MAC Computers</a></li>
+                <li class="icon-circle"><a href="#">Console Repair</a></li>
+                <li class="icon-circle"><a href="#">Smartphone Repair</a></li>
+                <li class="icon-circle"><a href="#">Data Recovery</a></li>
+                <li class="icon-circle"><a href="#">Laptop Macbook Repair</a></li>
+                <li class="icon-circle"><a href="#">Tablet Repair</a></li>
+            </ul>
+        </div>
+    </div>
+    
+    <div class="row newsletter">
+        <div class="col-md-6 newsletter-left">
+            <h3>Get The Newsletter</h3>
+            <p>Subscribe to our newsletter to receive news & updates. We promise to not spam you, super promise!</p>
+        </div>
+        <div class="col-md-6 newsletter-right">
+            <input type="email" placeholder="E-Mail Address">
+            <button>SUBSCRIBE</button>
+        </div>
+    </div>
+
+    <div class="row footer-bottom">
+        <div class="col-md-8">
+            <p>Copyright © 2023 Fixit Digital Repair Store</p>
+        </div>
+        <div class="col-md-4">
+            <div class="icon">
+                <a href="#" class="footer-icon">
+                    <i class="fa fa-facebook"></i>
+                </a>
+                <a href="#" class="footer-icon">
+                    <i class="fa fa-twitter"></i>
+                </a>
+                <a href="#" class="footer-icon">
+                    <i class="fa fa-google-plus"></i>
+                </a>
+                <a href="#" class="footer-icon">
+                    <i class="fa fa-instagram"></i>
+                </a>
+                <a href="#" class="footer-icon">
+                    <i class="fa fa-youtube"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ front/labs4/3192-content.php
@@ -0,0 +1,39 @@
+<div class="container mt-5">
+    <div class="text-center mb-5">
+        <h6 class="website-title">BUILD YOUR WEBSITE</h6>
+        <h2 class="list-style-left">List Style Left</h2>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-4">
+            <h3 class="list-title">ICONS LIST</h3>
+            <ul class="icon-list">
+                <li><i class="fas fa-chart-pie"></i> Doctors and Medical Staff</li>
+                <li><i class="fas fa-tree"></i>Patient and Visitor Guide </li>
+                <li><i class="fas fa-drum"></i> Patient Online Services</li>
+                <li><i class="fas fa-dragon"></i> Patient Care and Health</li>
+                <li><i class="fas fa-ethernet"></i> Review your Case Documents</li>
+            </ul>
+        </div>
+        <div class="col-md-4">
+            <h3 class="list-title1">SQUARE LIST</h3>
+            <ul class="square-list">
+                <li class="list-item">Doctors and Medical Staff</li>
+                <li class="list-item">Patient and Visitor Guide</li>
+                <li class="list-item">Patient Online Services</li>
+                <li class="list-item">Patient Care and Health</li>
+                <li class="list-item">Review your Case Documents</li>
+            </ul>
+        </div>
+        <div class="col-md-4">
+            <h3 class="list-title1">CIRCLE LIST</h3>
+            <ul class="circle-list">
+                <li class="list-item">Doctors and Medical Staff</li>
+                <li class="list-item">Patient and Visitor Guide</li>
+                <li class="list-item">Patient Online Services</li>
+                <li class="list-item">Patient Care and Health</li>
+                <li class="list-item">Review your Case Documents</li>
+            </ul>
+        </div>
+    </div>
+</div>
